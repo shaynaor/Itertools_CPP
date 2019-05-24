5f9504aa9675a46c93bdd7c20d1e184c1aec7504
shaynaor <shaynaor10@gmail.com>
2019-05-24 15:32:02 +0300

    Added const iterator

--- zip.hpp
+++ zip.hpp
@@ -39,6 +39,11 @@ namespace itertools{
                     iterator(typename T::iterator item1,typename T::iterator item1End , typename U::iterator item2)
                     : _it1(item1), _it1End(item1End), _it2(item2) {}
 
+                    /* Copy constructor */
+                    iterator(const iterator &other)
+                    : _it1(other._it1), _it1End(other._it1End), _it2(other._it2) {}
+                    
+
                     /* Operator * implementation. */
 		            auto operator*()  {
                         return pair(*(_it1),*(_it2));
@@ -75,14 +80,81 @@ namespace itertools{
 
             }; // End itetator class.
 
+
+
+            /* cont_Iterator inner  class */
+            class const_iterator{
+
+                private:
+                    /* Private data-members. */
+                    typename T::const_iterator _it1;
+                    typename T::const_iterator _it1End;
+                    typename U::const_iterator _it2;
+
+                public:
+                    /* Constructor. */
+                    const_iterator(typename T::const_iterator item1,typename T::const_iterator item1End , typename U::const_iterator item2)
+                    : _it1(item1), _it1End(item1End), _it2(item2) {}
+
+                    /* Copy constructor */
+                    const_iterator(const const_iterator &other)
+                    :_it1(other._it1), _it1End(other._it1End), _it2(other._it2) {}
+
+                    /* Operator * implementation. */
+		            const auto operator*() const {
+                        return pair(*(_it1),*(_it2));
+		            }
+
+                    /* Operator -> implementation. */
+		            const auto operator->() const {
+                       return pair(&(_it1),&(_it2));
+		            }
+                    
+                    /* Prefix operator ++ implementation.(++i)  */
+		            const_iterator& operator++() {
+                        _it1++;
+                        _it2++;
+			            return *this;
+		            }
+
+                    /* Postfix operator ++ implementation.(i++)  */ 
+		            const const_iterator operator++(int) {
+                        const_iterator it = *this; 
+                         ++*this; 
+                        return it; 
+		            }
+
+                    /* Operator == implementation. Return true iff _it == rhs._it */
+                    bool operator==(const const_iterator& rhs) const {
+			            return (_it1 == rhs._it1) && (_it2 == rhs._it2);
+		            }
+
+                    /* Operator != implementation. Return true iff _it != rhs._it */
+		            bool operator!=(const const_iterator& rhs) const {
+			            return (_it1 != rhs._it1) && (_it2 != rhs._it2);
+                    }
+
+            }; // End const_itetator class.
+
+
             /* Returns iterator to range begin. */
             typename zip::iterator begin() {
 		        return zip::iterator(_item1.begin(),_item1.end() ,_item2.begin());
 	        }
 
+            /* Returns const iterator to range begin. */
+            const typename zip::const_iterator begin() const {
+		        return zip::const_iterator(_item1.begin(),_item1.end() ,_item2.begin());
+	        }
+
             /* Returns iterator to range end. */
 	        typename zip::iterator end() {
 		        return zip::iterator(_item1.end(), _item1.end(), _item2.end());
+            }
+
+            /* Returns const iterator to range end. */
+	        const typename zip::const_iterator end() const {
+		        return zip::const_iterator(_item1.end(), _item1.end(), _item2.end());
             }
 
     }; // End zip class.
@@ -98,7 +170,7 @@ class pair {
     public:
 
         /* Constructor. */
-        pair(T first, U second):
+        pair(const T first, const U second):
         _first(first), _second(second) {}
 
 
