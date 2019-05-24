2f68472010fb751eb75bfecee57b300fbbdf1997
shaynaor <shaynaor10@gmail.com>
2019-05-24 15:51:52 +0300

    Added const iterator to powerset class

--- powerset.hpp
+++ powerset.hpp
@@ -34,6 +34,10 @@ namespace itertools{
                     iterator(typename T::iterator it)
                     : _it(it) {}
 
+                    /* Copy constructor. */
+                    iterator(const iterator& other)
+                    : _it(other._it) {}
+
                     /* Operator * implementation. */
 		            auto operator*()  {
                         std::string s = "{}";
@@ -66,14 +70,88 @@ namespace itertools{
 
             }; // End class iterator
 
+
+
+
+
+
+
+
+            /* const_iterator inner  class */
+            class const_iterator{
+
+                private:
+                    /* Private data-members. */
+                    typename T::const_iterator _it;
+                
+                public:
+
+                    /* Constructor. */
+                    const_iterator(typename T::const_iterator it)
+                    : _it(it) {}
+
+                    /* Copy constructor. */
+                    const_iterator(const const_iterator& other)
+                    : _it(other._it) {}
+
+                    /* Operator * implementation. */
+		            const auto operator*() const {
+                        std::string s = "{}";
+                        return s;
+		            }
+
+                    /* Prefix operator ++ implementation.(++i)  */
+		            const_iterator& operator++() {
+                        _it++;
+			            return *this;
+		            }
+
+                    /* Postfix operator ++ implementation.(i++)  */ 
+		            const const_iterator operator++(int) {
+                        const_iterator it = *this; 
+                         ++*this; 
+                        return it; 
+		            }
+
+                    /* Operator == implementation. Return true iff _it == rhs._it */
+                    bool operator==(const const_iterator& rhs) const {
+			            return (_it == rhs._it);
+		            }
+
+                    /* Operator != implementation. Return true iff _it != rhs._it */
+		            bool operator!=(const const_iterator& rhs) const {
+			            return (_it != rhs._it);
+                    }
+
+
+            }; // End class const_iterator
+
+
+
+
+
+
+
+
+
              /* Returns iterator to range begin. */
             typename powerset::iterator begin() {
 		        return powerset::iterator(_item.begin());
 	        }
 
+             /* Returns const iterator to range begin. */
+            const typename powerset::const_iterator begin() const {
+		        return powerset::const_iterator(_item.begin());
+	        }
+
             /* Returns iterator to range end. */
 	        typename powerset::iterator end() {
 		        return powerset::iterator(_item.end());
+            }
+
+            /* Returns const iterator to range end. */
+	        const typename powerset::const_iterator end() const {
+		        return powerset::const_iterator(_item.end());
             }
 
 
